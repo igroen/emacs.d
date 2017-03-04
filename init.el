187d1b2a1d5639a8a55430a131d9ea79d8402d84
Iwan in 't Groen <iwan@Iwans-MacBook-Air.local>
2017-03-04 20:29:18 +0100

    Bind C-x C-f to helm-find-files

--- init.el
+++ init.el
@@ -38,6 +38,7 @@
 (global-set-key (kbd "M-x")     'helm-M-x)
 (global-set-key (kbd "C-x b")   'helm-mini)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 ;; Helm fuzzy matching
 (setq helm-M-x-fuzzy-match                  t
