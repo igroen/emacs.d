5a9af4eb49b50802821d729d10a41733e6a80809
Iwan in 't Groen <iwanintgroen@gmail.com>
2017-09-15 18:59:52 +0200

    Save desktop session data to current directory

--- init.el
+++ init.el
@@ -34,14 +34,6 @@
 (setq auto-save-list-file-prefix nil)
 
 ;; Automatically save and restore sessions
-(setq desktop-dirname             "~/.emacs.d/desktop/"
-      desktop-base-file-name      "emacs.desktop"
-      desktop-base-lock-name      "lock"
-      desktop-path                (list desktop-dirname)
-      desktop-save                t
-      desktop-files-not-to-save   "^$" ;reload tramp paths
-      desktop-load-locked-desktop nil
-      desktop-auto-save-timeout   30)
 (desktop-save-mode 1)
 
 ;; Enable ido mode and flx-ido
