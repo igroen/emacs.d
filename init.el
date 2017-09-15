e08b647d689a9788a05741d765a8ab04e809e45d
Iwan in 't Groen <iwanintgroen@gmail.com>
2017-09-15 18:30:44 +0200

    Enable desktop-save-mode

--- init.el
+++ init.el
@@ -33,6 +33,17 @@
 ;; Backups are placed in backups folder by better-defaults plugin
 (setq auto-save-list-file-prefix nil)
 
+;; Automatically save and restore sessions
+(setq desktop-dirname             "~/.emacs.d/desktop/"
+      desktop-base-file-name      "emacs.desktop"
+      desktop-base-lock-name      "lock"
+      desktop-path                (list desktop-dirname)
+      desktop-save                t
+      desktop-files-not-to-save   "^$" ;reload tramp paths
+      desktop-load-locked-desktop nil
+      desktop-auto-save-timeout   30)
+(desktop-save-mode 1)
+
 ;; Enable ido mode and flx-ido
 (ido-mode 1)
 (setq ido-everywhere 1)
