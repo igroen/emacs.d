ea42544332a82ae80a8443ee35f878517e973339
Iwan in 't Groen <iwanintgroen@gmail.com>
2018-10-16 01:48:35 +0200

    Move complete config to settings.org

--- init.el
+++ init.el
@@ -6,16 +6,6 @@
 
 ;;; Code:
 
-;; Set load-path for libraries
-(add-to-list
- 'load-path
- (expand-file-name "lisp" user-emacs-directory))
-
-(load-library "tweak-startup-time")
-
-;; Initialize use-package
-(load-library "init-use-package")
-
 (org-babel-load-file (expand-file-name "settings.org" user-emacs-directory))
 
 ;;; init.el ends here
